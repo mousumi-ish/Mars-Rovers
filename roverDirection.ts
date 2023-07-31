e31e4055966c2a450388716ab2f6f953a12f5f2e
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-01 00:15:59 +0100

    feat: updated roverdiection

--- roverDirection.ts
+++ roverDirection.ts
@@ -4,3 +4,23 @@ export enum Direction {
   South = "S",
   West = "W",
 }
+export const rightMovement = (direction: Direction) => {
+  const rightmove = new Map<Direction, Direction>([
+    [Direction.North, Direction.East],
+    [Direction.East, Direction.South],
+    [Direction.South, Direction.West],
+    [Direction.West, Direction.North],
+  ]);
+
+  return rightmove.get(direction);
+};
+export const leftMovement = (direction: Direction) => {
+  const leftmove = new Map<Direction, Direction>([
+    [Direction.North, Direction.West],
+    [Direction.West, Direction.South],
+    [Direction.South, Direction.East],
+    [Direction.East, Direction.North],
+  ]);
+
+  return leftmove.get(direction);
+};
