e1cf13d6b53f64bad55023b6be1d770172eba243
mousumi-ish <baral_mousumi@yahoo.com>
2023-07-31 20:47:03 +0100

    feat: created files with positions of rover and direction it is facing

---
+++ rover.ts
@@ -0,0 +1,7 @@
+import { Direction } from "./roverDirection";
+
+export type Rover = {
+  x: number;
+  y: number;
+  facingTowards: Direction;
+};
---
+++ roverDirection.ts
@@ -0,0 +1,6 @@
+export enum Direction {
+  North = "N",
+  East = "E",
+  South = "S",
+  West = "W",
+}
