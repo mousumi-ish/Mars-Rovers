7d92bb7c1875bb8ad7a473e0511ae3ffeb95fb71
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-02 20:41:25 +0100

    feat: initial setup

--- leftmove.ts
+++
@@ -1,9 +0,0 @@
-import { leftMovement } from "./roverDirection";
-import { Rover } from "./rover";
-import { Action } from "./roverAction";
-
-export const rotateLeft: Action = (rover: Rover): Rover => ({
-  ...rover,
-
-  facingTowards: leftMovement(rover.facingTowards),
-});
--- rightmove.ts
+++
@@ -1,8 +0,0 @@
-import { rightMovement } from "./roverDirection";
-import { Rover } from "./rover";
-import { Action } from "./roverAction";
-
-export const rotateRight: Action = (rover: Rover): Rover => ({
-  ...rover,
-  facingTowards: rightMovement(rover.facingTowards),
-});
