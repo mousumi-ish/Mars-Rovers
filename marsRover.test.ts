8fec5c3f728699e5955b9a5d094d27a7b0da8eac
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-03 00:23:37 +0100

    passed the test for coordinate(1,2) and facing towards North

--- marsRover.test.ts
+++ marsRover.test.ts
@@ -17,3 +17,19 @@ describe("Moving rover", () => {
     }).toThrow("Obstacles , can't move");
   });
 });
+//checking for input 1,2 N and the direction of movement LMLMLMLMMM
+
+test("Move rover on the plateau for given co-ordinates and direction", () => {
+  let plateau = new Plateau(5, 5);
+
+  let rover = new Rover(
+    new newPosition(new Position(1, 2), Direction.N),
+    plateau
+  );
+
+  let command = new Command(rover);
+
+  expect(command.execute("LMLMLMLMM")).toEqual(
+    new newPosition(new Position(1, 3), Direction.N)
+  );
+});
